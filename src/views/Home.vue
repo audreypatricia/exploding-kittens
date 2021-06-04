ef60f03b96b159a60caa327cfbd499425cbfca65
Audrey Patricia <audreypatricia@Audreys-MacBook-Pro.local>
2021-06-04 10:56:19 +1000

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Hello World! It's time to play exploding kittens"/>
   </div>
 </template>
 
